635c1bbdb800b1172522cd6d56360bad79f83b94
godfriedpadia <godfriedpadia@gmail.com>
2025-03-06 23:27:21 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int x = 5;
+    char str[] = "Hello, World!";
+    printf("%d\n", x);
+    printf("%s\n", str);
+    return 0;
+}
