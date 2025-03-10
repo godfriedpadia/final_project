652ed09ba7ed2ffaab14e688a05225f052534fc6
godfriedpadia <godfriedpadia@gmail.com>
2025-03-10 07:07:09 +0000

    Add 6.c

---
+++ src/6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x > 3) {
+        printf("x is greater than 3");
+    } else {
+        printf("x is not greater than 3");
+    }
+    return 0;
+}
