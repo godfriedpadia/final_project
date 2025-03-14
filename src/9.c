8430fc898b0b613aeea316a0d3bcd58e69dfe7a8
godfriedpadia <godfriedpadia@gmail.com>
2025-03-14 22:38:11 +0000

    Add 9.c

---
+++ src/9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("X is positive");
+    } else {
+        printf("X is negative");
+    }
+    return 0;
+}
